8d52a567b918e5a6b36a93ba2352868364c4839c
suraj-9849 <surajlohit42@gmail.com>
2025-12-11 13:58:00 +0530

    FontFamily

--- ph_frontend/app/layout.tsx
+++ ph_frontend/app/layout.tsx
@@ -1,12 +1,6 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './globals.css';
 import { AuthProvider } from '@/contexts/auth-context';
-
-const inter = Inter({
-  variable: '--font-inter',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'PharmaCare - Pharmacy Management System',
@@ -20,7 +14,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} font-sans antialiased`}>
+      <head>
+        <style>
+          @import
+          url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
+        </style>
+      </head>
+      <body
+        className={`
+        inter font-sans antialiased`}
+      >
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
